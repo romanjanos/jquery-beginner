0f513c9fa8144c9e36e8931ecfdb19a4b7b540e1
Janos <romanjanos0504@gmail.com>
2021-05-06 13:26:57 +0200

    JSON

--- js/main.js
+++ js/main.js
@@ -1,24 +1,128 @@
 $(() => {
-    
+    // Global functions.
+    const set = (key, value) => localStorage.setItem(key, value);
+    const get = key => localStorage.getItem(key);
+    const increase = el => set(el, parseInt(get(el), 10), +1);
+    const decrease = el => set(el, parseInt(get(el), 10), -1);
+
+    // Global variables
+    const Game = $('#g');
+    let settings = {};
+    let startGame = 0;
+    let level = '';
+    let timer = 0;
+
+    const startScreen = (text) => {
+        Game
+            .removeAttr('class')
+            .empty();
+        $('.logo').fadeIn(250);
+
+        $('.c1').text(text.substring(0, 1));
+        $('.c2').text(text.substring(1, 2));
+        $('.c3').text(text.substring(2, 3));
+        $('.c4').text(text.substring(3, 4));
+    }
+
+    // Init cards and set events.
+    const initCards = () => {
+        $('.logo .card:not(".twist")').on('click', (e) => {
+            e.preventDefault();
+            $(e.currentTarget)
+                .toggleClass('active')
+                .siblings('.card')
+                .not('.twist')
+                .removeClass('active');
+
+            if ($(e.target).is('.playnow')) {
+                $('.logo .card')
+                    .last()
+                    .addClass('active')
+                    .siblings('.card')
+                    .not('.twist')
+                    .removeClass('active');
+            }
+        });
+    };
+
+
     $(window).on('keyup', (e) => {
         // Pause. (p)
         if (e.keyCode === 80) {
-            
-            
+            if (Game.attr('data-paused') === '1') {
+                Game.attr('data-paused', '0');
+                $('.timer').css('animation-play-state', 'running')
+                $('.pause').remove();
+            } else {
+                Game.attr('data-paused', '1');
+                $('.timer').css('animation-play-state', 'paused');
+                Game.after('<div class="pause"></div>');
+            }
+
         } else if (e.keyCode === 27) {
             // Escape. (esc)
-            
-            
-        } else if (e.keyCode === 83) {
-            // Slide. (s)
-            
-            
-        } else if (e.keyCode === 66) {
-            // (b)
-           
-            
+            startScreen('flip');
+            if (Game.attr('data-paused') === '1') {
+                Game.attr('data-paused', '0');
+                $('.pause').remove();
+            }
+            $(window).off();
         }
     });
+
+    // Init timer.
+    const initTimer = (timer) => {
+        $('<i class="timer"></i>')
+            .prependTo(Game)
+            .css({
+                animation: `timer ${timer}ms linear`
+            })
+            .one('webkitAnimationEnd oanimationend msAnimationEnd animationend', e => {
+                startScreen('fail');
+            });
+    }
+
+    // Start Game
+    $('.play').on('click', (ev) => {
+        ev.preventDefault();
+        increase('flip_abandoned');
+        $('.info').fadeOut();
+
+        level = $(ev.currentTarget).data('level');
+        timer = settings.difficulties[level].time * 1000;
+
+        Game.addClass(level);
+
+        $('.logo').fadeOut(250, () => {
+            initBoard();
+        });
+
+        //Start timer.
+        initTimer(timer);
+    });
+
+    // Init game board.
+    const initBoard = () => {
+        startGame = $.now();
+        const obj = [];
+
+        // Create and shuffle cards.
+        for (let i = 0; i < settings.difficulties[level].cards; i++) {
+            obj.push(i);            
+        }
+    };
+
+
+    // Init game.
+    (() => {
+        startScreen('flip');
+
+        $.getJSON('http://localhost:3000/settings', (jsonSettings) => {
+            settings = jsonSettings;
+            initCards();
+        });
+
+    })();
 });
 
 
